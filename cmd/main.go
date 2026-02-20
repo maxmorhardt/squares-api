0a2a8ba451ebc761d25dc5382781bf2f2b0fca42
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2026-02-20 08:52:53 -0500

    source in logs

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
+	"path/filepath"
 
 	_ "github.com/maxmorhardt/squares-api/docs"
 	"github.com/maxmorhardt/squares-api/internal/bootstrap"
@@ -10,7 +12,15 @@ import (
 
 func init() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		Level: slog.LevelInfo,
+		Level:     slog.LevelInfo,
+		AddSource: true,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.SourceKey {
+				source := a.Value.Any().(*slog.Source)
+				a.Value = slog.StringValue(fmt.Sprintf("%s:%d", filepath.Base(source.File), source.Line))
+			}
+			return a
+		},
 	}))
 	slog.SetDefault(logger)
 	logger.Info("initialized logger")
